50b11578720a9c2e17a66e74a7bddb79d29ff048
Ersin Çöl <ersincol235@gmail.com>
2024-06-04 11:37:18 +0300

    Create basic math operation functions and testing browser's console

---
+++ main.js
@@ -0,0 +1,20 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+console.log(add(10, 5));
+console.log(subtract(10, 5));
+console.log(multiply(10, 5));
+console.log(divide(10, 5));
