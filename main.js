bbd7802e4f8904b5090f7f68abec57768714f9ad
Ersin Çöl <ersincol235@gmail.com>
2024-06-04 13:20:45 +0300

    Create a basic HTML calculator and Create a new function operate

--- main.js
+++ main.js
@@ -14,7 +14,24 @@ function divide(number1, number2) {
   return number1 / number2;
 }
 
-console.log(add(10, 5));
-console.log(subtract(10, 5));
-console.log(multiply(10, 5));
-console.log(divide(10, 5));
+let firstNumber, operator, secondNumber;
+
+function operate(operator, firstNumber, secondNumber) {
+  let solution;
+
+  switch (operator) {
+    case "+":
+      solution = add(firstNumber, secondNumber);
+      break;
+    case "-":
+      solution = subtract(firstNumber, secondNumber);
+      break;
+    case "*":
+      solution = multiply(firstNumber, secondNumber);
+      break;
+    case "/":
+      solution = divide(firstNumber, secondNumber);
+  }
+
+  return solution;
+}
